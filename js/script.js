04cecb9efa422aa271a831ec37b954023d787008
Edir P Santos <fennder@gmail.com>
2025-03-29 10:24:52 -0300

    Start create site for bio social media

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll('.links a');
+
+links.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.transform = 'scale(1.1)';
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.transform = 'scale(1)';
+    });
+});
